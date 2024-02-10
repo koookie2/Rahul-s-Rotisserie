19a8ad11526aeb62ac892fc793ba41965e0a9902
kookie2 <kavinjgodwin@gmail.com>
2024-02-10 16:23:55 -0500

    nav_bar

--- counter.js
+++ counter.js
@@ -1,1 +1,12 @@
 alert("Hello");
+
+function change_nav() {
+    if (document.getElementById("side_menu_1").style.width == "0px")
+        document.getElementById("side_menu_1").style.width = "250px";
+    else
+        document.getElementById("side_menu_1").style.width = "0px";
+}
+
+document.getElementById("openBtn").onclick = function() {
+    change_nav();
+}
