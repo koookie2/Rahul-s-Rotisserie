23c28a31e54f5c96dd1494b440499154356d4b77
kookie2 <kavinjgodwin@gmail.com>
2024-02-10 21:21:38 -0500

    commit

--- counter.js
+++ counter.js
@@ -1,18 +1,19 @@
-let counter = 0;
-let increment = 10;
-let boolean1 = true;
+var counter = 0;
+var increment = 10;
+var booleans = [true, true, true, true, true, true, true];
 
-function buy(button, amount) {
+function buy(button, amount, boolean) {
     if (counter >= amount) {
         document.getElementById(button).style.opacity = 0;
         
-        boolean1 = false;
+        booleans[boolean] = false;
         counter -= amount;
         document.getElementById("counter").textContent = counter;
     }
 }
 
 function change_nav(button) {
+
     if (document.getElementById(button).style.width == "0px")
         document.getElementById(button).style.width = "250px";
     else
@@ -20,25 +21,57 @@ function change_nav(button) {
 }
 
 document.getElementById("button1").onclick = function() {
-    if (boolean1)
-        buy("button1", 10);
+    if (booleans[0])
+        buy("button1", 10, 0);
     else
         change_nav("sideMenu1");
 }
 
 document.getElementById("button2").onclick = function() {
-    if (boolean2)
-        buy("button2", 100);
+    if (booleans[1])
+        buy("button2", 100, 1);
     else
-        change_nav();
+        change_nav("sideMenu2");
+}
+
+document.getElementById("button3").onclick = function() {
+    if (booleans[2])
+        buy("button3", 1000, 2);
+    else
+        change_nav("sideMenu3");
+}
+
+document.getElementById("button4").onclick = function() {
+    if (booleans[3])
+        buy("button4", 10000, 3);
+    else
+        change_nav("sideMenu4");
+}
+
+document.getElementById("button5").onclick = function() {
+    if (booleans[4])
+        buy("button5", 100000, 4);
+    else
+        change_nav("sideMenu5");
+}
+
+document.getElementById("button6").onclick = function() {
+    if (booleans[5])
+        buy("button6", 1000000, 5);
+    else
+        change_nav("sideMenu6");
+}
+
+document.getElementById("button7").onclick = function() {
+    if (booleans[6])
+        buy("button7", 10000000, 6);
+    else
+        change_nav("sideMenu7");
 }
 
 
-function incrementCounter() {
-    counter += increment;
-    document.getElementById("counter").textContent = counter;
-}
 
 document.getElementById("main_button").onclick = function() {
-    incrementCounter();
+    counter += increment;
+    document.getElementById("counter").textContent = counter;
 }
